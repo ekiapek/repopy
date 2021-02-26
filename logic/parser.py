dd7fec1f27bf210d70c534e3e5c026929eef2865
ekiapek <eki_yeheskiel@hotmail.co.id>
2021-02-26 18:22:55 +0700

    modify parser to use astroid

--- logic/parser.py
+++ logic/parser.py
@@ -1,22 +1,30 @@
-from redbaron import RedBaron
+import astroid
 import glob
+import sys
+import traceback
 
 def parseCode(base_dir):
     # root_dir needs a trailing slash (i.e. /root/dir/)
     for filename in glob.iglob(base_dir + '**/*.py', recursive=True):
-        with open(filename) as source_code:
-            red = RedBaron(source_code.read())
-            a = red.find_all('ClassNode')
-            if(len(a)>0):
-                print("\n"+filename)
-            for x in a:
-                if(len(x.inherit_from)>0):                    
-                    print("Class "+x.name+" Parent:")
-                    for i in x.inherit_from:
-                        print(i.value)
-                else:
-                    print("Class "+x.name)
-                    pass
-    # pass
+        try:
+            file = open(filename).read()
+            code = astroid.parse(file)
+            for node in code.body:
+                if(isinstance(node,astroid.ClassDef)):
+                    print("\n"+filename)
+                    if(len(node.bases)>0):
+                        print(node.name+" Parent: ")
+                        for base in node.bases:
+                            if(isinstance(base,astroid.Attribute)):
+                                print(base.attrname)
+                            else:
+                                print(base.name)
+                    else:
+                        print(node.name)
+        except Exception as e:
+            print("ERROR:")
+            print(e)
+            traceback.print_tb(e.__traceback__)
+        # pass
 
 parseCode('C:\\Users\\ASUS\\Documents\\repopy\\')
