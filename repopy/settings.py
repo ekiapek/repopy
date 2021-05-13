52a66f4e10e39a5ade963cb2eafb1b30e9f2acb2
Yeheskiel Reza Andrazein <51573814+ekiapek@users.noreply.github.com>
2021-05-14 04:38:58 +0900

    [API] search endpoint done

--- repopy/settings.py
+++ repopy/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+from redisearch.client import Client
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,12 +136,14 @@ REPO_EXTRACTED_PATH = "repositories"
 APPEND_SLASH = True
 MEDIA_ROOT = os.path.join(os.path.expanduser("~"), REPO_BASE_PATH)
 API_URL = "http://127.0.0.1:8000/api/"
+RESERVED_KEYWORDS = [":related-with",":parent-of",":child-of",":function",":in"]
 
 #redis
 REDIS_HOST = '192.168.8.105'
 REDIS_PORT = 6379
 import redis
 REDIS_INSTANCE = redis.Redis(host=REDIS_HOST,port=REDIS_PORT, db=0)
+REDISEARCH_CLIENT = None
 
 #Cache
 CACHES = {
