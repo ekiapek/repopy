f46dd43abf920d219527f36b66d9fa99527f20c6
Yeheskiel Reza Andrazein <51573814+ekiapek@users.noreply.github.com>
2021-05-14 17:00:31 +0900

    modify settings.py

--- repopy/settings.py
+++ repopy/settings.py
@@ -17,7 +17,7 @@ import django_heroku
 from redisearch.client import Client
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -128,10 +128,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "statics"),
+    os.path.join(BASE_DIR, "static"),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
